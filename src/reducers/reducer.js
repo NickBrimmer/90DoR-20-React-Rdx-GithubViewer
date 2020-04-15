ee3684a68a98918351deac461604f93c3034a150
Nick <nickbrimmer@gmail.com>
2020-04-15 16:15:27 -0700

    created actions and reducer to be added to the global context.

---
+++ src/reducers/reducer.js
@@ -0,0 +1,18 @@
+
+
+const initialState = {
+  loading: false,
+  userName: '',
+  userInfo: ''
+};
+
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    
+
+    default:
+      return state;
+  }
+}
+
