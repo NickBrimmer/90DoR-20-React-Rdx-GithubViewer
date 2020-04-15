c234b7007d67d44c20b7476c860a6c3d3a3cc242
Nick <nickbrimmer@gmail.com>
2020-04-15 16:17:07 -0700

    finished the switch statement where the switches were determined by action.type and the payload is delivered to the global context state

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,3 +1,4 @@
+import { FETCH_USER_LOADING, FETCH_USER } from '../actions/actions';
 
 
 const initialState = {
@@ -9,7 +10,10 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
-    
+    case FETCH_USER_LOADING:
+      return { ...state, loading: true };
+    case FETCH_USER:
+      return { ...state, loading: false, userInfo: action.payload };
 
     default:
       return state;
