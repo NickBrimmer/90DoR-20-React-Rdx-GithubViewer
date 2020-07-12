e3119ab9c0511a928a5bf31ed3239aeb6b1dd169
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-07-11 17:19:21 -0700

    changed header image and added an scss file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.css';
 const Header = () => {
   return (
     <>
-      <img className={styles.headerImg} src='src/assets/Screenshot 2020-02-18 16.45.41.png' />
+      <img className={styles.headerImg} src='src/assets/GithubViewerHeader.jpg' />
       <h1 className={styles.hdrText} >Welcome to Github Viewer</h1>
       <h2 className={styles.subHeaderText} >Search any user by their Github username!</h2>
     </>
