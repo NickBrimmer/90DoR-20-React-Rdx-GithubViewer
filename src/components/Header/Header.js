e34d9ec271207afd3b2e31c4031ed7d72b70ff7d
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-07-12 19:42:48 -0700

    removing header image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import styles from './Header.css';
 const Header = () => {
   return (
     <>
-      <img className={styles.headerImg} src='src/assets/GithubViewerHeader.jpg' />
       <h1 className={styles.hdrText} >Welcome to Github Viewer</h1>
       <h2 className={styles.subHeaderText} >Search any user by their Github username!</h2>
     </>
